189efb90d307c4415b7bb1571133cff7eaa35cb7
misterkc007 <babakasse@yahoo.fr>
2017-02-02 11:46:40 +0100

    version 1 fonctionnelle

---
+++ app/scripts/controllers/carrosserie.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularAppApp.controller:CarrosserieCtrl
+ * @description
+ * # CarrosserieCtrl
+ * Controller of the angularAppApp
+ */
+angular.module('angularAppApp')
+  .controller('CarrosserieCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/moteur.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularAppApp.controller:MoteurCtrl
+ * @description
+ * # MoteurCtrl
+ * Controller of the angularAppApp
+ */
+angular.module('angularAppApp')
+  .controller('MoteurCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
